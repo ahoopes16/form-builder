362de555fcbc74c6ecb90348ca165629bd9f168a
Kevin Hoopes <kevin.alex.hoopes@gmail.com>
2018-07-29 16:43:12 -0500

    Initial commit with form_builder Python script

---
+++ form_builder.py
@@ -0,0 +1,13 @@
+"""This script will take in a series of labels and the type of
+input they should be. It will generate a valid HTML file of
+the form in question.
+@author: Kevin Hoopes
+@version: July 29, 2018
+"""
+
+def main():
+    """The main function that runs the script."""
+
+    print("We're going to build a form in HTML!")
+
+main()
